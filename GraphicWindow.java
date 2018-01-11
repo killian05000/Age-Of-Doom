6137d37aaeb07d53438b2d91fa06d8330a38df59
killian05000 <killianwolfger@hotmail.fr>
2018-01-11 06:24:47 +0100

    affichage

--- GraphicWindow.java
+++ GraphicWindow.java
@@ -8,8 +8,12 @@ import javax.swing.JTextField;
 
 public class GraphicWindow extends JFrame implements ActionListener
 {
-  private JTextField jtf_path;
-  private JLabel display_path;
+  private String folder1_path;
+  private String folder2_path;
+  private JLabel display_folder1_path;
+  private JLabel display_folder2_path;
+	private JButton load_folder1;
+  private JButton load_folder2;
 
   GraphicWindow(String s)
   {
@@ -18,19 +22,12 @@ public class GraphicWindow extends JFrame implements ActionListener
     setLocationRelativeTo(null);
     setResizable(false);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    display_path = new JLabel("hhhhhhhhhhhhhhhhhhhhhhhhhh");
-    jtf_path = new JTextField(40);
-    add(display_path);
-    add(jtf_path);
-
-    // JPanel panel = new JPane();
-    // panel.setLayout(new FlowLayout());
-    // panel.add(display_path);
 
     JMenuBar menuBar = new JMenuBar();
-    JMenu menu = new JMenu("Folder");
     JMenuItem item;
     setJMenuBar(menuBar);
+
+    JMenu menu = new JMenu("Folder");
     menuBar.add(menu);
 
       item = new JMenuItem("Load");
@@ -46,44 +43,79 @@ public class GraphicWindow extends JFrame implements ActionListener
       menu.add(item);
       item.addActionListener(this);
 
-    menu = new JMenu("Color");
-    menuBar.add(menu);
+    setContentPane(buildContentPane());
+  }
 
-      item = new JMenuItem("Black");
-      menu.add(item);
-      item.addActionListener(this);
+  private JPanel buildContentPane()
+  {
+	JPanel panel = new JPanel();
+	panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+  panel.add(Box.createRigidArea(new Dimension(0,5)));
+  panel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 
-      item = new JMenuItem("Red");
-      menu.add(item);
-      item.addActionListener(this);
 
-      item = new JMenuItem("Blue");
-      menu.add(item);
-      item.addActionListener(this);
-      menu.add(new JSeparator());
+  display_folder1_path = new JLabel("First folder's path");
+  load_folder1 = new JButton("Load1");
+  load_folder1.addActionListener(this);
 
-      item = new JMenuItem("Pick");
-      menu.add(item);
-      item.addActionListener(this);
+  display_folder2_path = new JLabel("First folder's path");
+  load_folder2 = new JButton("Load2");
+  load_folder2.addActionListener(this);
+
+  JButton validation = new JButton("ok");
+  validation.addActionListener(this);
+
+  JLabel display_duplicates = new JLabel("here will be the duplicates");
+
+
+	panel.add(display_folder1_path);
+  panel.add(load_folder1);
+  panel.add(display_folder2_path);
+  panel.add(load_folder2);
+  panel.add(validation);
+  panel.add(display_duplicates);
+
+	return panel;
   }
 
   public void actionPerformed(ActionEvent evenement)
   {
               ///////// MENU FILE /////////
 
-    if (evenement.getActionCommand().equals("Load"))
+    if (evenement.getActionCommand().equals("Load1"))
     {
       JFileChooser selector = new JFileChooser();
-      selector.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+      selector.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 
       if (selector.showOpenDialog(this) == JFileChooser.APPROVE_OPTION)
       {
         File file = selector.getSelectedFile();
-        display_path.setText(file.getPath());
-        jtf_path.setText(file.getPath());
+        display_folder1_path.setText(file.getPath());
         System.out.println("Chosen file : " + file);
       }
     }
+
+    if (evenement.getActionCommand().equals("Load2"))
+    {
+      JFileChooser selector = new JFileChooser();
+      selector.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+      if (selector.showOpenDialog(this) == JFileChooser.APPROVE_OPTION)
+      {
+        File file = selector.getSelectedFile();
+        display_folder2_path.setText(file.getPath());
+        System.out.println("Chosen file : " + file);
+      }
+    }
+
+    if (evenement.getActionCommand().equals("ok"))
+    {
+      folder1_path = display_folder1_path.getText();
+      folder2_path = display_folder2_path.getText();
+      System.out.println(folder1_path+" # "+folder2_path);
+      display_duplicates.setText("les voici : ");
+    }
+
     if (evenement.getActionCommand().equals("Save"))
     {
       System.out.println("MENU SAVE");
@@ -97,25 +129,6 @@ public class GraphicWindow extends JFrame implements ActionListener
           System.out.println("MENU QUIT");
           System.exit(0);
         }
-    }
-
-              ///////// MENU COLOR /////////
-
-    if (evenement.getActionCommand().equals("Black"))
-    {
-      System.out.println("MENU2 BLACK");
-    }
-    if (evenement.getActionCommand().equals("Red"))
-    {
-      System.out.println("MENU2 RED");
-    }
-    if (evenement.getActionCommand().equals("Blue"))
-    {
-      System.out.println("MENU2 BLUE");
-    }
-    if (evenement.getActionCommand().equals("Pick"))
-    {
-      System.out.println("MENU2 PICK");
     }
   }
 }
